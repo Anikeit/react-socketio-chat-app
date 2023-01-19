ea953e2abb9368040cc7a75392f5c9cd6134c758
Anikeit Saxena <64261003+Anikeit@users.noreply.github.com>
2023-01-19 16:42:17 +0530

    Major changes...code refactor and features added

--- server/index.js
+++ server/index.js
@@ -1,39 +1,51 @@
-const express = require("express");
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
+
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
+const router = require('./router');
+
 const app = express();
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-app.use(cors());
-
 const server = http.createServer(app);
+const io = socketio(server);
 
-app.get('/', (req, res) => {
-  res.sendStatus(200)
-})
+app.use(cors());
+app.use(router);
 
-let port =  process.env.PORT || 3001;
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-const io = new Server(server, {
-  cors: {
-    origin: "https://master--classy-liger-d9aa45.netlify.app/",
-    methods: ["GET", "POST"],
-  },
+    if(error) return callback(error);
+
+    socket.join(user.room);
+
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
 });
 
-io.on("connection", (socket) => {
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-  });
-
-  socket.on("disconnect", () => {
-  });
-});
-
-server.listen(port, () => {
-  console.log("Server is Running on",port)
-})
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
